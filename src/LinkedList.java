e1dad6fd04aa57bd58f074ad77388b57320417e1
SantiagoGrade <santiago54911@gmail.com>
2023-09-19 10:05:57 -0500

    as

---
+++ src/LinkedList.java
@@ -0,0 +1,106 @@
+public class LinkedList {
+    class Node {
+        int value;
+        Node next = null;
+        Node(int value){
+            this.value = value;
+        }
+    }
+    protected Node head = null;
+    protected Node tail = null;
+
+    public void agregarValorAlInicio(int value){
+        Node nuevoNodo = new Node(value);
+        nuevoNodo.next =  head;
+        head = nuevoNodo;
+        if(nuevoNodo.next == null){
+            tail = nuevoNodo;
+        }
+    }
+
+    public void agregarTail(int value){
+        Node nuevoNodo =new Node(value);
+        if(tail == null){
+            head = nuevoNodo;
+        }else{
+            tail.next = nuevoNodo;
+        }
+    }
+
+    public void imprimirLinkedList() {
+        Node valor = head;
+        while (valor != null) {
+            System.out.print(valor.value + " ");
+            valor = valor.next;
+        }
+        System.out.println();
+    }
+
+    public void agregarPorPosicion(int posicion, int valor) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if (posicion == 0) {
+            agregarValorAlInicio(valor);
+        } else {
+            Node node = new Node(valor);
+            Node actual = head;
+            for (int i = 0; i < posicion - 1; i++) {
+                if (actual == null) {
+                    throw new IndexOutOfBoundsException();
+                }
+                actual = actual.next;
+            }
+            //[0] -> [3] -> [5] -> [10]
+            if (actual.next == null) {
+                agregarTail(valor);
+            } else {
+                //[0] -> [3] -> [99]->[5]  -> [10]
+                node.next = actual.next;
+                actual.next = node;
+            }
+        }
+    }
+
+    public Node buscarPorPosicion(int posicion) {
+        if (posicion < 0) {
+            throw new IndexOutOfBoundsException();
+        } else if(posicion == 0) {
+            return head;
+        } else {
+            Node actual = head;
+            for (int i = 0; i < posicion; i++) {
+
+                if(i == posicion - 1){
+                    return actual;
+                }
+
+                Node node = actual.next;
+                actual = node;
+
+            }
+        }
+        return null;
+    }
+
+    public boolean buscarPorContenido(int contenido) {
+
+        Node actual = head;
+        int buscar = actual.value;
+        boolean cursor = true;
+
+        while (cursor == true) {
+            if (buscar == contenido) {
+                break;
+            } else if(actual == tail){
+                cursor = false;
+                break;
+            } else if(buscar != contenido ){
+                Node nodo = actual.next;
+                actual = nodo;
+                buscar = actual.value;
+            }
+        }
+        return cursor;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,26 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.agregarValorAlInicio(33);
+        linkedList.agregarValorAlInicio(13);
+        linkedList.agregarValorAlInicio(23);
+        linkedList.agregarValorAlInicio(16);
+
+        linkedList.agregarPorPosicion(2,543);
+        linkedList.imprimirLinkedList();
+
+        System.out.println(linkedList.buscarPorPosicion(2).value);
+        System.out.println(linkedList.buscarPorPosicion(4).value);
+
+        boolean a = linkedList.buscarPorContenido(222);
+
+        if (a == true){
+            System.out.println("El valor se encuentra en la linkedlist");
+        }else {
+            System.out.println("El valor no se encuentra en la linkedlist");
+        }
+    }
+}
